74f439cba38baf4fe89bb2db34857bddc781f71a
yasirosyokuhin <fujiitky@gmail.com>
2021-01-08 16:12:42 +0900

    add

--- serach-horse.js
+++ serach-horse.js
@@ -2,4 +2,4 @@ let getHorselist = document.getElementById('getHorselist');
 let horselist = document.getElementById('horselist');
 
 
-document.getElementById('horselist').innerHTML += 'ここにリストを記載'
+horselist.innerHTML +='ここにリストを記載';
